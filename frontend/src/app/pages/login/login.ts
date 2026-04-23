f132d6d543798eba5ed14c8e7a8d1c249543bddb
alexnavarro <alex.navarro22@lacetania.cat>
2026-04-23 17:19:02 +0200

    frontend register y login 2

--- frontend/src/app/pages/login/login.ts
+++ frontend/src/app/pages/login/login.ts
@@ -1,18 +1,27 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { AuthService } from '../../services/auth';
 
 @Component({
   selector: 'app-login',
   imports: [FormsModule],
   templateUrl: './login.html',
-  styleUrl: './login.css',
-  styles: [
-    "node_modules/bootstrap/dist/css/bootstrap.min.css"
-  ]
+  styleUrl: './login.css'
 })
 export class Login {
   email: string = "";
   password: string = ""
 
+  constructor(private authService: AuthService){}
 
- }
+  login(){
+    const data = {
+      email:this.email,
+      password:this.password
+    }
+
+    this.authService.login(data).subscribe(res =>{
+      console.log(res)
+    })
+  }
+}
---
+++ frontend/src/app/services/auth.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  private apiUrl = 'http://localhost:8000/api'
+
+  constructor(private http:HttpClient) {}
+
+  register(data:any){
+    return this.http.post(`${this.apiUrl}/register`, data)
+  }
+
+  login(data:any){
+    return this.http.post(`${this.apiUrl}/login`, data)
+  }
+}
